437ded33e1ef40e8cf6d3cf7535ec723e4121fe0
Muhammad Khalad <khaladjaan09@gmail.com>
2024-11-20 19:19:44 +0500

    changes 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,37 @@
-$(document).ready(function(e){
+$(document).ready(function () {
+    const headerTop = $(".header-top");
+    const navbar = $("#header .navbar");
+    const sections = $("section"); // Adjust selector if necessary
 
+    function updateScrollMargin() {
+        let navbarHeight = navbar.outerHeight(true); // Get the navbar height
+        let headerTopHeight = headerTop.is(":visible") ? headerTop.outerHeight(true) : 0;
+        let scrollMarginTop = navbarHeight + headerTopHeight;
 
-	$(window).on("scroll", function () {
-		if(matchMedia("only screen and (min-width: 1200px)").matches && $(window).scrollTop() >= ($(".header-top").outerHeight(true)+120))
-		{
-			$(".header-top").hide().fadeOut(300);
-			$("#header .navbar").addClass("fixed-navigation").animate();
-			
-		}
-		else if(matchMedia("only screen and (min-width: 1200px)").matches)
-		{
-			$(".header-top").show().fadeIn(300);
-			$("#header .navbar").removeClass("fixed-navigation").animate();
-		}
-		else if(!matchMedia("only screen and (min-width: 1200px)").matches && $(window).scrollTop() >= ($("#header .navbar").outerHeight(true)+120))
-		{
-			$("#header .navbar").addClass("fixed-navigation").animate();
-		}
-		else if(!matchMedia("only screen and (min-width: 1200px)").matches)
-		{
-			$("#header .navbar").removeClass("fixed-navigation").animate();
-		}
-	});
+        sections.each(function () {
+            $(this).css("scroll-margin-top", scrollMarginTop + "px");
+        });
+    }
 
-          
-          
+    // Run the function on scroll and resize
+    $(window).on("scroll resize", function () {
+        if (window.matchMedia("only screen and (min-width: 1200px)").matches) {
+            if ($(window).scrollTop() >= headerTop.outerHeight(true) + 120) {
+                headerTop.hide().fadeOut(300);
+                navbar.addClass("fixed-navigation");
+            } else {
+                headerTop.show().fadeIn(300);
+                navbar.removeClass("fixed-navigation");
+            }
+        } else if ($(window).scrollTop() >= navbar.outerHeight(true) + 120) {
+            navbar.addClass("fixed-navigation");
+        } else {
+            navbar.removeClass("fixed-navigation");
+        }
+
+        updateScrollMargin(); // Adjust scroll margin dynamically
+    });
+
+    // Initial setup
+    updateScrollMargin();
 });
